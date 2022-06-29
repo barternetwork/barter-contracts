ff6145b401939eb4ba78391b90c9ad57c197420b
974984977@qq.com <974984977@qq.com>
2022-06-29 11:23:36 +0800

    upd interface

---
+++ contracts/interface/IBalancerSwap/ISignaturesValidator.sol
@@ -0,0 +1,19 @@
+
+
+
+
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.0;
+
+interface ISignaturesValidator {
+    /**
+     * @dev Returns the EIP712 domain separator.
+     */
+    function getDomainSeparator() external view returns (bytes32);
+
+    /**
+     * @dev Returns the next nonce used by an address to sign messages.
+     */
+    function getNextNonce(address user) external view returns (uint256);
+}
