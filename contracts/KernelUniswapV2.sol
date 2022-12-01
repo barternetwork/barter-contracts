1a750774df9bf63cfe05a914b1abafc6612b6797
974984977@qq.com <974984977@qq.com>
2022-12-01 21:04:09 +0800

    init

--- contracts/KernelUniswapV2.sol
+++ contracts/KernelUniswapV2.sol
@@ -15,24 +15,33 @@ import "./libs/SafeMath.sol";
 contract kernelUniSwapV2 {
 
     address public constant UNI_SWAP = 0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D;
-        
-    function filterSwap(uint256  amountInArr,uint256  amountOutMinArr,bytes memory pathArr,address to,uint256 _deadLine, address inputAddre,address outAddre) external  payable{
-            swapInputV2(amountInArr,amountOutMinArr,pathArr,to,_deadLine,inputAddre,outAddre);       
+        // 
+
+
+    function filterSwap(bytes memory exchangeData) external  payable{
+            uint256 amountInArr;
+            uint256 amountOutMinArr;
+            address[] memory pathArr;
+            address to;
+            uint256 deadLines;
+            address inputAddre;
+            address outAddre;
+            (amountInArr,amountOutMinArr,pathArr,to,deadLines,inputAddre,outAddre) = abi.decode(exchangeData,(uint256,uint256,address[],address,uint256,address,address));
+            swapInputV2(amountInArr,amountOutMinArr,pathArr,to,deadLines,inputAddre,outAddre);              
     }
  
 
      // v2 
-    function  swapInputV2(uint256 _amountInArr,uint256 _amountOutMinArr,bytes memory _path,address _to,uint256 _deadLine,address _inputAddre ,address _outAddre) internal{
+    function  swapInputV2(uint256 _amountInArr,uint256 _amountOutMinArr,address[] memory _path,address _to,uint256 _deadLine,address _inputAddre ,address _outAddre) internal{
                     uint[] memory amounts;
-                    address[] memory pathArrs  = abi.decode(_path,(address[]));
                     if(_inputAddre == address(0)){
-                        amounts = IBarterswapV2Router01(UNI_SWAP).swapExactETHForTokens{value:_amountInArr}(_amountOutMinArr,pathArrs,_to,_deadLine);
+                        amounts = IBarterswapV2Router01(UNI_SWAP).swapExactETHForTokens{value:_amountInArr}(_amountOutMinArr,_path,_to,_deadLine);
                     }else if(_outAddre == address(0)){
                           TransferHelper.safeApprove(_inputAddre,UNI_SWAP,_amountInArr);
-                        amounts = IBarterswapV2Router01(address(UNI_SWAP)).swapExactTokensForETH(_amountInArr,_amountOutMinArr,pathArrs,_to,_deadLine);
+                        amounts = IBarterswapV2Router01(address(UNI_SWAP)).swapExactTokensForETH(_amountInArr,_amountOutMinArr,_path,_to,_deadLine);
                     }else{
                         TransferHelper.safeApprove(_inputAddre,UNI_SWAP,_amountInArr);
-                        amounts = IBarterswapV2Router01(address(UNI_SWAP)).swapExactTokensForTokens( _amountInArr, _amountOutMinArr,pathArrs,_to,_deadLine);
+                        amounts = IBarterswapV2Router01(address(UNI_SWAP)).swapExactTokensForTokens( _amountInArr, _amountOutMinArr,_path,_to,_deadLine);
                 }
             }
 
