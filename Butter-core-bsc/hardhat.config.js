de192440f247f8f414c6f2fb9c8d94c6f059dacb
974984977@qq.com <974984977@qq.com>
2022-12-15 15:11:31 +0800

    upd butter name

--- Butter-core-bsc/hardhat.config.js
+++ Butter-core-bsc/hardhat.config.js
@@ -2,6 +2,9 @@ require("@nomicfoundation/hardhat-toolbox");
 
 
 
+// hardhat config
+
+
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.17",
--- Butter-core-polygon /hardhat.config.js
+++ Butter-core-polygon /hardhat.config.js
@@ -1,6 +1,7 @@
 require("@nomicfoundation/hardhat-toolbox");
 
 
+// hardhat config
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
